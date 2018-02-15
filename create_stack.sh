0bda7e75ee856daf481b7e0ae8c4fcf10581ad5f
Wouter Mellema <wouter1mellema@gmail.com>
2018-02-15 16:26:50 +0100

    Hopefully fixed escaping issues on windows, commiting to test

--- create_stack.sh
+++ create_stack.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 root=$(pwd);
 
-git clone https://github.com/laravel/laravel
-wait
-cd laravel;
-wait
+git clone https://github.com/laravel/laravel;
+wait;
+cd "laravel";
+wait;
 sed 's/DB_DATABASE=homestead/DB_DATABASE=default/g' .env.example > .envtmp1;
-wait
+wait;
 sed 's/DB_USERNAME=homestead/DB_USERNAME=default/g' .envtmp1 > .env;
-wait
+wait;
 rm -f .envtmp*;
-wait
+wait;
 git clone https://github.com/Laradock/laradock.git;
-wait
+wait;
 git pull;
-wait
-cd laradock;
-wait
+wait;
+cd "laradock";
+wait;
 cp env-example .env;
-wait
+wait;
 pwd;
-wait
+wait;
 docker-compose up -d nginx mysql;
-wait
-mysql_host=$(cd $root/laravel/laradock; docker-compose exec mysql ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+wait;
+mysql_host=$(cd "$root/laravel/laradock"; docker-compose exec mysql ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 wait
 sed -i 's/DB_HOST=127.0.0.1/DB_HOST='$mysql_host'/g' "$root/laravel/.env"
 wait
