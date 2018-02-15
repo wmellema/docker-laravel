2dd0220c684e246094614094c3a43b76b434f328
Wouter Mellema <wouter1mellema@gmail.com>
2018-02-15 16:21:00 +0100

    Hopefully fixed escaping issues on windows, commiting to test

--- create_stack.sh
+++ create_stack.sh
@@ -25,13 +25,13 @@ docker-compose up -d nginx mysql;
 wait
 mysql_host=$(cd $root/laravel/laradock; docker-compose exec mysql ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 wait
-sed -i 's/DB_HOST=127.0.0.1/DB_HOST='$mysql_host'/g' $root/laravel/.env
+sed -i 's/DB_HOST=127.0.0.1/DB_HOST='$mysql_host'/g' "$root/laravel/.env"
 wait
-cd $root/laravel/laradock;
+cd "$root/laravel/laradock";
 wait
 docker-compose exec workspace composer install
 wait
-cd $root/laravel/laradock;
+cd "$root/laravel/laradock";
 wait
 docker-compose exec workspace php artisan key:generate
 wait
