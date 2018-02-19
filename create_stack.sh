e01697a20a7b265a638e16b38322fd09998a1938
Wouter Mellema <wouter1mellema@gmail.com>
2018-02-19 10:56:03 +0100

    Added command to only install Portainer

--- create_stack.sh
+++ create_stack.sh
@@ -8,6 +8,12 @@ case $1 in
     docker volume create portainer_data
     docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
     ;;
+  portainer-only)
+    echo Only creating portainer
+    docker volume create portainer_data
+    docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+    exit 0
+    ;;
   *)
     echo Not installing Portainer
     ;;
