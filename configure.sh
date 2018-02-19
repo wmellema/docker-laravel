c76605a2ead0d0ed194efc0718811f818fda0b2e
Wouter Mellema <wouter1mellema@gmail.com>
2018-02-19 19:51:46 +0100

    Added some more logging for different distros

--- configure.sh
+++ configure.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 user=$(logname)
+version=$(./version.sh)
 
-dnf remove -y docker docker-common docker-selinux docker-engine-selinux docker-engine
-dnf -y install dnf-plugins-core
-dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-dnf install -y docker-ce docker-compose
+case $version in
+  Fedora)
+    dnf remove -y docker docker-common docker-selinux docker-engine-selinux docker-engine
+    dnf -y install dnf-plugins-core
+    dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+    dnf install -y docker-ce docker-compose
+    ;;
+  *)
+    echo Unsupported version! Please install docker-ce and docker-compose manually
+    exit 1
+    ;;
+esac
 groupadd docker
 usermod -aG docker $user
 systemctl enable docker
