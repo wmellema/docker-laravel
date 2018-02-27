5c95537785ff51cb3bc219a69f15ee9adf9ad4b7
Wouter Mellema <wouter1mellema@gmail.com>
2018-02-27 10:17:40 +0100

    Fixed Ubuntu ip  command

--- create_stack.sh
+++ create_stack.sh
@@ -158,7 +158,7 @@ if [ $system = "Darwin" ];
   mysql_host=$(cd "$root/laravel/laradock"; docker-compose exec mysql ip -4 addr show eth0 | awk '/inet /{print substr($2,0,length($2)-3)}')
 elif [ $system = "Ubuntu" ];
 then
-	mysql_host=$(cd "$root/laravel/laradock"; docker-compose exec mysql ip -4 addr show eth0 | awk '/inet addr/{print substr($2,6)}')
+	mysql_host=$(cd "$root/laravel/laradock"; docker-compose exec mysql hostname -i)
 else
   mysql_host=$(cd "$root/laravel/laradock"; docker-compose exec mysql ip -4 addr show eth0 | awk '/inet /{print substr($2,0,length($2)-3)}')
 fi
