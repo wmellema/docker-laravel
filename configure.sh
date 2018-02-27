8ed333aceeb54e7c7e66e6585bee3ed7ca46359d
Wouter Mellema <wouter1mellema@gmail.com>
2018-02-27 10:00:01 +0100

    Added Ubuntu configuration

--- configure.sh
+++ configure.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
-user=$(logname)
 version=$(./version.sh)
 
 case $version in
   Fedora)
+    user=$(logname)
     dnf remove -y docker docker-common docker-selinux docker-engine-selinux docker-engine
     dnf -y install dnf-plugins-core
     dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
     dnf install -y docker-ce docker-compose
+    ;;
+  Ubuntu)
+    user=$SUDO_USER
+    apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    apt-get update
+    apt-get install -y docker-ce docker-compose
     ;;
   *)
     echo Unsupported version! Please install docker-ce and docker-compose manually
     exit 1
     ;;
 esac
+echo "Configuring user for using docker"
 groupadd docker
 usermod -aG docker $user
+echo "Configuring docker service"
 systemctl enable docker
 systemctl start docker
